a18c8c118b81ffc4d8026249ae7ee838e67ed117
DatKHWOLF <dkhwolf@gmail.com>
2024-05-07 14:25:35 +0200

    heute was erledigt?

--- mylist/views.py
+++ mylist/views.py
@@ -7,6 +7,7 @@ def mylist(request):
         print('Received data : ', request.POST['itemName'])
         #adding items to database
         ShoppingItem.objects.create(name = request.POST['itemName'] )
-
-
-    return render(request, 'shoplist.html')
+    all_items = ShoppingItem.objects.all()
+    # ShoppingItem.objects.get()/filter() sind auch sinnvolle Varianten
+    print (all_items)
+    return render(request, 'shoplist.html', {'all_items' : all_items})
