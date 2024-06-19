3796a8d4e521a60838dfdda71bce561b6f17665f
DatKHWOLF <dkhwolf@gmail.com>
2024-06-19 11:38:58 +0200

    bissl weiter

--- shopping_list/urls.py
+++ shopping_list/urls.py
@@ -15,11 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from mylist.views import mylist
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',mylist),
     path('mylist/', mylist ),
+    path('articles/', include('articles.urls'))
 ]
+urlpatterns+=staticfiles_urlpatterns()
